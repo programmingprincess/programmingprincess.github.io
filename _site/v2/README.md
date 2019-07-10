929b4abdd3bf055d738e2d619a748e3936fda8da
programmingprincess <jiaqiwu@asu.edu>
2019-07-09 23:56:07 -0700

    add node modules lol

---
+++ _site/v2/README.md
@@ -0,0 +1,11 @@
+# jiaqiwu.com
+
+Personal website hosted on GitHub Pages. Using:
+* Bootstrap and jQuery (general)
+* Animate.css for icon animations 
+* Font-awesome for icons 
+* Wow.js for icon loading 
+
+
+### Previously: jiaqiwu.me 
+
---
+++ _site/v2/js/olsjs.js
@@ -0,0 +1,245 @@
+jQuery(document).ready(function($) {
+    //make navbar fixed when the top of screen hits
+    var offset = $('.navbar .nav').offset().top;
+    $(window).bind('scroll', function() {
+        if($(window).width() >= 768) {   //not mobile
+            if ($(window).scrollTop() >= offset) {
+                $('.navbar').addClass('navbar-fixed-top solid-nav');
+            } else {
+                $('.navbar').removeClass('navbar-fixed-top solid-nav');
+            }
+        } else if($(window).width() < 768) {   //mobile
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.navbar').removeClass('solid-nav');
+        }
+
+        //when you hit the bottom of the page
+        if($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $('.navbar').removeClass('solid-nav');
+        }
+    }); //end of scroll function
+
+    //navbar when resizing window
+    $( window ).resize(function() {
+        if($(window).width() >= 768) {   //not mobile
+            $('.menu__list').removeClass('mobile-nav');
+            $('.navbar').removeClass('solid-nav');
+        } else {    //mobile
+            $('.navbar').removeClass('solid-nav');
+        }
+    });
+
+    //mobile nav stuff
+    $(".navbar-nav li a").click(function(event) {   //close when you click on link
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(".navbar-brand").click(function(event) {   //close when you click on home link
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    //close navbar when link is pressed
+    $(document).click(function(e) {
+        if (!$(e.target).is('a') && !$(e.target).hasClass('navbar-toggle')) {
+            $('.collapse').collapse('hide');
+        }
+
+        if($('.navbar-collapse').attr('aria-expanded') == 'true') {
+            $('.menu__list').addClass('mobile-nav');
+        } else {
+            $('.menu__list').removeClass('mobile-nav');
+        }
+    });
+
+    $('.navbar-toggle').on('click', function () {
+        $('.menu__list').addClass('mobile-nav');
+    });
+
+    //scroll "easing" when clicked
+    $(function() {
+        $('a.menu-link').bind('click', function(e) {
+            var $target = $(this);
+            var offset;
+            //this extra clause accounts for the scroll indicator that gets in the way of about navbar
+            offset = $($target.attr('href')).offset().top;
+            $('html, body').stop().animate({
+                scrollTop: offset
+            }, 1500, 'easeInOutExpo');
+            e.preventDefault();
+        });
+        $('#scrolldown').bind('click', function(e) {
+            $('html, body').stop().animate({
+                scrollTop: $('#about').offset().top
+            }, 1500, 'easeInOutExpo');
+            e.preventDefault();
+        });
+        $('#scrollup, .navbar-brand').bind('click', function(e) {
+            $('html, body').stop().animate({
+                scrollTop: 0
+            }, 1500, 'easeInOutExpo');
+            e.preventDefault();
+        });
+    });
+
+    $(window).on('scroll', function () {
+        var sections = $('section'),
+            nav = $('nav'),
+            position = $(this).scrollTop();
+
+        sections.each(function() {
+            var top = $(this).offset().top - nav.outerHeight(),
+                bottom = top + $(this).outerHeight();
+
+            if($(window).scrollTop() + $(window).height() == $(document).height()) {
+                console.log("bottom!");
+                nav.find('a').removeClass('active');
+                nav.find('a[href="#contact"]').addClass("active");
+            } else if (position >= top && position <= bottom) {
+                nav.find('a').removeClass('active');
+                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+            }
+        });
+    });
+
+    //check if mobile
+    if($(window).width() > 580) {
+        var pause = 20;
+        var speed = 8;
+        var lines = [
+        'cout <<meta/><<meta/> <span style="color:#E7D76D;">"Hello, world!"</span>;', 
+        ' <span style="color:#72CFEC;"><i>System</i></span><span style="color:#be4678;">.</span>out<span style="color:#be4678;">.</span>println(<span style="color:#FFEB62;">"Hello, world!"</span>);', 
+        'print <span style="color:#E7D76D;">"Hello, world!"</span>',
+        'console<span style="color:#72CFEC;">.log</span>(<span style="color:#E7D76D;">"Hello, world!"</span>);',
+        'cat(<span style="color:#E7D76D;">"Hello, world!"</span>)',
+        ' <span style="color:#72CFEC;">echo</span><span style="color:#E7D76D;"> \'Hello, world!\'</span>;'
+        ];
+
+        var line = lines[0];
+        var cur = 0,
+        dir = 1,
+        lang = 0;
+        
+
+        var s = 0;
+
+        setInterval(function loop() {
+        cur += dir;
+
+        //parser: if erasing
+        if(dir < 0) {
+            if (line[cur] == '>') {
+                while(line[cur] != '<')
+                    cur--;
+            }
+            cur += dir;
+            if (line[cur] == '>') {
+                while(line[cur] != '<')
+                    cur--;
+            }
+            cur += dir;
+            if (line[cur] == '>') {
+                while(line[cur] != '<')
+                    cur--;
+            }
+        } 
+
+        //parser: if typing
+        else {    
+            if (line[cur] == '<') {
+                while(line[cur] != '>')
+                    cur++; 
+            }
+        }
+        
+        //erasing 
+        if (cur < 0) {
+          cur = 0;
+          dir = -dir;
+          lang = (++lang) % lines.length;
+          line = lines[lang];
+        }
+
+        //line has been typed out
+        if (cur > line.length) {
+          cur = line.length;
+
+          //loop until pause time is up
+          if (s++ > pause) {
+            s = 0
+            dir = -dir;
+          }
+        } 
+
+        document.querySelector('#type').innerHTML = line.substring(0, cur);
+        }, 500 / speed)
+
+        //typewriter effect configurations
+        /*var str,
+            i = 0,
+            isTag,
+            text;
+
+        str = "<h3><p class ='line1'> &lt;!DOCTYPE = HTML&gt;<br>";
+        str += "&lt;html&gt;</p>";
+        str += "<p class='line3'>&lt;head&gt;</p>";
+        str += "<p class='line4'>&lt;title&gt; <span style='color:#E7E7E7;'>Welcome to my page! </span>&lt;/title&gt;";
+
+        document.getElementById('typewriter').innerHTML = "";
+
+        (function type() {
+            text = str.slice(0, ++i);
+            if (text == str) {
+                document.getElementById('typewriter').innerHTML = text + '<span class = "dash" style = "margin-left: 2px"></span></h3>';
+                return;
+            }
+            var char = text.slice(-1);
+            if( char === '&') {
+                i+=3;
+                text = str.slice(0, i);
+                document.getElementById('typewriter').innerHTML = text + '<span class = "dash"></span></h3>';
+            } else if( char == '<' ) {
+                isTag = true;
+            } else if( char == '>' ) {
+                isTag = false;
+            }
+
+            document.getElementById('typewriter').innerHTML = text + '<span class = "dash"></span></h3>';
+
+            if (isTag) return type();
+            setTimeout(type, 80);
+
+
+        }());*/
+    }
+
+        //portfolio configurations
+    $('#portfolio .buttons li').click(function() {
+        $(this).css('outline', 'none');
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        var filter = $(this).text().toLowerCase().replace(' ',
+            '-');
+        if (filter == 'all') {
+            $('#portfolio .tile li.hidden').removeClass(
+                'hidden').css('display', 'inline-block');
+        } else {
+            $('#portfolio .tile li').each(function() {
+                if (!$(this).hasClass(filter)) {
+                    $(this).addClass(
+                        'hidden');
+                } else {
+                    $(this).hide(); //hide elements to add fade effect if they were prexisting on page
+                    $(this).removeClass(
+                        'hidden').css('display',
+                        'inline-block');
+                }
+            });
+        }
+        return false;
+    });
+
+}); //end of document ready function
+
+
+//initialize animated icons with WOW.js
+new WOW().init();
